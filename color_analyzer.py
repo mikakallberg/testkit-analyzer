3fa925a71931559a792648c769a38a8a702865cb
mikakallberg <mikaela.kallberg@hotmail.com>
2022-12-12 00:58:25 +0100

    Solve BGR2RGB in image and print

--- color_analyzer.py
+++ color_analyzer.py
@@ -1,27 +1,19 @@
 # from collections import Counter
 # from sklearn.cluster import KMeans
 # from matplotlib import colors
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
 
-# def prep_image(raw_img):
-#     modified_img = cv2.resize(raw_img, (900, 600), interpolation = cv2.INTER_AREA)
-#     modified_img = modified_img.reshape(modified_img.shape[0]*modified_img.shape[1], 3)
-#     return modified_img
+img = cv2.imread('test_image.jpg')
+image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 
-# def color_analysis(img):
-#     clf = KMeans(n_clusters = 5)
-#     color_labels = clf.fit_predict(img)
-#     center_colors = clf.cluster_centers_
-#     counts = Counter(color_labels)
-#     ordered_colors = [center_colors[i] for i in counts.keys()]
-#     hex_colors = [rgb_to_hex(ordered_colors[i]) for i in counts.keys()]
-#     plt.figure(figsize = (12, 8))
-#     plt.pie(counts.values(), color_labels = hex_colors, colors = hex_colors)
-#     print(hex_colors)
+cv2.imshow('Image', img)
+print(image)
+
+cv2.waitKey(0)
 
 # def create_bar(height, width, color):
 #     bar = np.zeros((height, width, 3), np.uint8)
@@ -30,7 +22,7 @@ import cv2
 #     print(red, green, blue)
 #     return bar, (red, green, blue)
 
-image = cv2.imread('test_image.jpg')
+
 # height, width, _ = np.shape(img)
 # print(height, width)
 # data = np.reshape(img, (height * width, 3))
@@ -41,8 +33,7 @@ image = cv2.imread('test_image.jpg')
 # flags = cv2.KMEANS_RANDOM_CENTERS
 # compactness, labels, centers = cv2.kmeans(data, number_clusters, None, criteria, 10, flags)
 # print(centers)
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-print(image)
+
 
 # font = cv2.FONT_HERSHEY_DUPLEX
 # bars = []
@@ -60,17 +51,36 @@ print(image)
 #         font, 0.5, (128, 128, 128), 1, cv2.LINE_AA)
 #     print(f'{index + 1}. RGB: {row}')
 
-plt.imshow(image)
+
 # cv2.imshow('Dominant colors', img_bar)
 # plt.imshow(image)s
-cv2.waitKey(0)
+
+
 
 # def rgb_to_hex(rgb_color):
 #     hex_color = "#"
 #     for i in rgb_color:
 #         i = int(i)
 #         hex_color += ("{:02x}".format(i))
+#     print(hex_color)
 #     return hex_color
+
+# def prep_image(raw_img):
+#     modified_img = cv2.resize(raw_img, (900, 600), interpolation = cv2.INTER_AREA)
+#     modified_img = modified_img.reshape(modified_img.shape[0]*modified_img.shape[1], 3)
+#     return modified_img
+
+
+# def color_analysis(img):
+#     clf = KMeans(n_clusters = 5)
+#     color_labels = clf.fit_predict(img)
+#     center_colors = clf.cluster_centers_
+#     counts = Counter(color_labels)
+#     ordered_colors = [center_colors[i] for i in counts.keys()]
+#     hex_colors = [rgb_to_hex(ordered_colors[i]) for i in counts.keys()]
+#     plt.figure(figsize = (12, 8))
+#     plt.pie(counts.values(), color_labels = hex_colors, colors = hex_colors)
+#     print(hex_colors)
 
 # modified_image = prep_image(image)
 # color_analysis(modified_image)
