c989398b04f365611dae7f9b9e8070a545edc7bf
mikakallberg <mikaela.kallberg@hotmail.com>
2022-12-22 15:56:26 +0100

    Put images in folder, problem with pie-chart

--- color_analyzer.py
+++ color_analyzer.py
@@ -1,19 +1,48 @@
-# from collections import Counter
-# from sklearn.cluster import KMeans
-# from matplotlib import colors
-# import matplotlib.pyplot as plt
+from collections import Counter
+from sklearn.cluster import KMeans
+from matplotlib import colors
+import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
 
+def rgb_to_hex(rgb_color):
+    hex_color = "#"
+    for i in rgb_color:
+        i = int(i)
+        hex_color += ("{:02x}".format(i))
+    print(hex_color)
+    return hex_color
+
+def prep_image(raw_img):
+    modified_img = cv2.resize(raw_img, (900, 600), interpolation = cv2.INTER_AREA)
+    modified_img = modified_img.reshape(modified_img.shape[0]*modified_img.shape[1], 3)
+    return modified_img
+
+def color_analysis(img):
+    clf = KMeans(n_clusters = 5)
+    color_labels = clf.fit_predict(img)
+    center_colors = clf.cluster_centers_
+    counts = Counter(color_labels)
+    ordered_colors = [center_colors[i] for i in counts.keys()]
+    hex_colors = [rgb_to_hex(ordered_colors[i]) for i in counts.keys()]
+    plt.figure(figsize = (12, 8))
+    plt.pie(counts.values(), color_labels = hex_colors, colors = hex_colors)
+    plt.savefig("color_analysis_report.png")
+    print(hex_colors)
+
+
 img = cv2.imread('test_image.jpg')
-image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+rgb_values = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+hls_value = cv2.cvtColor(img, cv2.COLOR_BGR2HLS_FULL)
+print(rgb_values)
+print(hls_value)
+modified_image = prep_image(img)
+color_analysis(modified_image)
+
+plt.imshow(img)
 
 
-cv2.imshow('Image', img)
-print(image)
-
-cv2.waitKey(0)
 
 # def create_bar(height, width, color):
 #     bar = np.zeros((height, width, 3), np.uint8)
@@ -22,9 +51,10 @@ cv2.waitKey(0)
 #     print(red, green, blue)
 #     return bar, (red, green, blue)
 
+# img = cv2.imread('test_image.jpg')
 
 # height, width, _ = np.shape(img)
-# print(height, width)
+
 # data = np.reshape(img, (height * width, 3))
 # data = np.float32(data)
 
@@ -32,9 +62,9 @@ cv2.waitKey(0)
 # criteria = (cv2.TERM_CRITERIA_EPS + cv2.TermCriteria_MAX_ITER, 10, 1.0)
 # flags = cv2.KMEANS_RANDOM_CENTERS
 # compactness, labels, centers = cv2.kmeans(data, number_clusters, None, criteria, 10, flags)
-# print(centers)
-
-
+# print('Dominant colors (BGR): ', centers)
+# rgb_values = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# print('Dominant colors (RGB): ', rgb_values)
 # font = cv2.FONT_HERSHEY_DUPLEX
 # bars = []
 # rgb_values = []
@@ -51,36 +81,14 @@ cv2.waitKey(0)
 #         font, 0.5, (128, 128, 128), 1, cv2.LINE_AA)
 #     print(f'{index + 1}. RGB: {row}')
 
+# # rgb_values = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# # hls_value = cv2.cvtColor(img, cv2.COLOR_BGR2HLS_FULL)
 
+# cv2.imshow('Image', img)
 # cv2.imshow('Dominant colors', img_bar)
+# # print('RGB values:', rgb_values)
+
+
+# cv2.waitKey(0)
+
 # plt.imshow(image)s
-
-
-
-# def rgb_to_hex(rgb_color):
-#     hex_color = "#"
-#     for i in rgb_color:
-#         i = int(i)
-#         hex_color += ("{:02x}".format(i))
-#     print(hex_color)
-#     return hex_color
-
-# def prep_image(raw_img):
-#     modified_img = cv2.resize(raw_img, (900, 600), interpolation = cv2.INTER_AREA)
-#     modified_img = modified_img.reshape(modified_img.shape[0]*modified_img.shape[1], 3)
-#     return modified_img
-
-
-# def color_analysis(img):
-#     clf = KMeans(n_clusters = 5)
-#     color_labels = clf.fit_predict(img)
-#     center_colors = clf.cluster_centers_
-#     counts = Counter(color_labels)
-#     ordered_colors = [center_colors[i] for i in counts.keys()]
-#     hex_colors = [rgb_to_hex(ordered_colors[i]) for i in counts.keys()]
-#     plt.figure(figsize = (12, 8))
-#     plt.pie(counts.values(), color_labels = hex_colors, colors = hex_colors)
-#     print(hex_colors)
-
-# modified_image = prep_image(image)
-# color_analysis(modified_image)
